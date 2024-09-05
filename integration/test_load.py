020fffe56328119daa10f7cb2d4d47537fb0d2c1
Giovanni Padilha <geovannepadilha@hotmail.com>
2024-09-05 16:14:00 -0300

    Boas práticas em testes, setup, teardown, parametrize e test reports

--- integration/test_load.py
+++ integration/test_load.py
@@ -1,11 +1,12 @@
 import pytest
 # Função da biblioteca subprocess para executar comandos CLI
 # do SO e o resultado do comando vem como binário.
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 
+# Adicionando os markers `integration` e `medium` para o testea abaixo.
 @pytest.mark.integration
 @pytest.mark.medium
-def test_load():
+def test_load_positive_call_load_command():
     """Test command load."""
     out = check_output(
         ["dundie", "load", "tests/assets/people.csv"]
@@ -13,3 +14,18 @@ def test_load():
     
     assert len(out) == 2
 
+
+# Adicionando os markers `integration` e `medium` para o testea abaixo.
+@pytest.mark.integration
+@pytest.mark.medium
+# Adicionando por meio de injeção de dependências uma lista de opções do comando `load`,
+# que o teste vai executar.
+@pytest.mark.parametrize("wrong_command", ["loady", "carrega", "start"])
+def test_load_negative_call_load_command_with_wrong_params(wrong_command):
+    """Test command load."""
+    with pytest.raises(CalledProcessError) as error:
+        out = check_output(
+            ["dundie", wrong_command, "tests/assets/people.csv"]
+        ).decode("utf-8").split("\n")
+
+    assert "status 2" in str(error.getrepr())
--- tests/test_load.py
+++ tests/test_load.py
@@ -2,9 +2,16 @@ import pytest
 from dundie.core import load
 from .constants import PEOPLE_FILE
 
+# Aplica os marcadores `unit` e `high` para o teste abaixo.
 @pytest.mark.unit
 @pytest.mark.high
-def test_load():
+def test_load_positive_has_2_people():
     """Test load function."""
     assert len(load(PEOPLE_FILE)) == 2
+    
+# Aplica os marcadores `unit` e `high` para o teste
+@pytest.mark.unit
+@pytest.mark.high
+def test_load_positive_has_first_name_starts_with_j():
+    """Test load function."""
     assert load(PEOPLE_FILE)[0][0] == "J"
